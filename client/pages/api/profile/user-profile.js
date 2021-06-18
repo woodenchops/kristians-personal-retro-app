f29167784f93b60bf397e007d6499223f0038a8a
Kristian Woods <kris_woods@hotmail.co.uk>
2021-06-18 17:16:33 +0100

    fix getServerSideProps by forcing headers

--- client/pages/api/profile/user-profile.js
+++ client/pages/api/profile/user-profile.js
@@ -1,41 +1,46 @@
-// import { getSession } from 'next-auth/client';
-// import { connectToDatabase } from '../../../helpers/db-util';
-// import { API_URL } from '../../../config/index';
+import { getSession } from 'next-auth/client';
+import { connectToDatabase } from '../../../helpers/db-util';
+import { API_URL } from '../../../config/index';
 
-// export async function handler(req, res) {
-//   if (req.method !== 'GET') {
-//     return res.status(422).json({ message: 'Invalid request' });
-//   }
+export async function handler(req, res) {
+  if (req.method !== 'GET') {
+    return res.status(422).json({ message: 'Invalid request' });
+  }
 
-//   console.log('api req', req.cookies);
+  console.log('api req', req);
 
-//   const session = await getSession({ req });
+  const session = await getSession({ req });
 
-//   console.log('HERE', session);
+  console.log('HERE', session);
 
-//   if (!session) {
-//     return res.status(401).json({ message: 'Not authenticated' });
-//   }
+  if (!session) {
+    return res.status(401).json({ message: 'Not authenticated' });
+  }
 
-//   const userEmail = session.user.email;
+  const userEmail = session.user.email;
 
-//   const client = await connectToDatabase(`${process.env.AUTH_DB}`);
+  const client = await connectToDatabase(`${process.env.AUTH_DB}`);
 
-//   const userCollection = client
-//     .db(`${process.env.AUTH_DB}`)
-//     .collection(`${process.env.USERS_COLLECTION}`);
+  const userCollection = client
+    .db(`${process.env.AUTH_DB}`)
+    .collection(`${process.env.USERS_COLLECTION}`);
 
-//   const user = await userCollection.findOne({ email: userEmail });
+  const user = await userCollection.findOne(
+    { email: userEmail },
+    { projection: { _id: 0, password: 0 } }
+  );
 
-//   if (!user) {
-//     client.close();
-//     return res.status(401).json({ message: 'User not found!' });
-//   }
+  if (!user) {
+    client.close();
+    return res.status(401).json({ message: 'User not found!' });
+  }
 
-//   client.close();
+  client.close();
 
-//   return res
-//     .status(200)
-//     .json({ message: 'Successfully found user profile', data: { user } });
-// }
-// export default handler;
+  console.log('USER', user);
+
+  return res
+    .status(200)
+    .json({ message: 'Successfully found user profile', response: { user } });
+}
+export default handler;
--- client/pages/profile/index.jsx
+++ client/pages/profile/index.jsx
@@ -1,86 +1,28 @@
-// import React from 'react';
-import { getSession, useSession } from 'next-auth/client';
-// import fetch from 'node-fetch';
-// import { API_URL } from '../../config/index';
-import Profile from '../../components/Profile';
-import { connectToDatabase } from '../../helpers/db-util';
-
-function ProfilePage({ user }) {
-  const [session, loading] = useSession();
-
-  // if (loading) return null;
-
-  // if (!loading && !session) return <p>Access Denied</p>;
-
-  return (
-    <div>
-      <div>
-        <Profile user={user} />
-      </div>
-    </div>
-  );
-}
-
-export async function getServerSideProps({ req }) {
-  const session = await getSession({ req });
-
-  if (!session) {
-    return {
-      redirect: {
-        destination: '/',
-        permanent: false,
-      },
-    };
-  }
-
-  const userEmail = session.user.email;
-
-  const client = await connectToDatabase(`${process.env.AUTH_DB}`);
-
-  const userCollection = client
-    .db(`${process.env.AUTH_DB}`)
-    .collection(`${process.env.USERS_COLLECTION}`);
-
-  const user = await userCollection.findOne(
-    { email: userEmail },
-    { projection: { _id: 0, password: 0 } }
-  );
-
-  client.close();
-
-  return {
-    props: { session, user },
-  };
-}
-
-export default ProfilePage;
-
-/** *********
- OLD WAY
- ********** */
-
-// import React, { useEffect, useState } from 'react';
+// // import React from 'react';
 // import { getSession, useSession } from 'next-auth/client';
-// import fetch from 'node-fetch';
-// import { API_URL } from '../../config/index';
+// // import fetch from 'node-fetch';
+// // import { API_URL } from '../../config/index';
 // import Profile from '../../components/Profile';
+// import { connectToDatabase } from '../../helpers/db-util';
 
 // function ProfilePage({ user }) {
 //   const [session, loading] = useSession();
 
+//   // if (loading) return null;
+
+//   // if (!loading && !session) return <p>Access Denied</p>;
+
 //   return (
 //     <div>
-//       <pre>{user}</pre>
-//       <div>{/* <Profile user={userDetails} /> */}</div>
+//       <div>
+//         <Profile user={user} />
+//       </div>
 //     </div>
 //   );
 // }
 
-// export async function getServerSideProps(context) {
-//   const session = await getSession(context);
-
-//   const res = await fetch(`${API_URL}/api/profile/user-profile`);
-//   const user = await res.json();
+// export async function getServerSideProps({ req }) {
+//   const session = await getSession({ req });
 
 //   if (!session) {
 //     return {
@@ -91,9 +33,73 @@ export default ProfilePage;
 //     };
 //   }
 
+//   const userEmail = session.user.email;
+
+//   const client = await connectToDatabase(`${process.env.AUTH_DB}`);
+
+//   const userCollection = client
+//     .db(`${process.env.AUTH_DB}`)
+//     .collection(`${process.env.USERS_COLLECTION}`);
+
+//   const user = await userCollection.findOne(
+//     { email: userEmail },
+//     { projection: { _id: 0, password: 0 } }
+//   );
+
+//   client.close();
+
 //   return {
 //     props: { session, user },
 //   };
 // }
 
 // export default ProfilePage;
+
+/** *********
+ OLD WAY
+ ********** */
+
+import React from 'react';
+import { getSession } from 'next-auth/client';
+import fetch from 'node-fetch';
+import { API_URL } from '../../config/index';
+import Profile from '../../components/Profile';
+
+function ProfilePage({ data }) {
+  const {
+    response: { user },
+  } = data;
+
+  return (
+    <div>
+      <div>
+        <Profile user={user} />
+      </div>
+    </div>
+  );
+}
+
+export async function getServerSideProps(ctx) {
+  const session = await getSession(ctx);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  }
+
+  const res = await fetch(`${API_URL}/api/profile/user-profile`, {
+    method: 'GET',
+    headers: ctx.req ? { cookie: ctx.req.headers.cookie } : undefined,
+  });
+  const data = await res.json();
+
+  return {
+    props: { session, data },
+  };
+}
+
+export default ProfilePage;
