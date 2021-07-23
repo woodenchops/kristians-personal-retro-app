61737510f26fb6d800bd309dc0ffba74c694aabd
Kristian Woods <kris_woods@hotmail.co.uk>
2021-07-23 10:02:54 +0100

    update tags

--- client/components/TopicTag.jsx
+++ client/components/TopicTag.jsx
@@ -3,17 +3,21 @@ import { FaHashtag } from 'react-icons/fa';
 import styles from '../styles/Tag.module.css';
 
 function TopicTag({ topics }) {
-  const formattedTopicTags = topics && topics.split(', ');
+  const formattedTopicTags =
+    topics &&
+    topics.split(',').filter((x) => {
+      return x.length != '' && x != ',' && /[a-zA-Z]/g.test(x);
+    });
 
   return (
     <>
-      {formattedTopicTags && (
+      {formattedTopicTags && formattedTopicTags.length > 0 && (
         <span className={styles.TagSpacing}>
           <span>Topics: </span>
           {formattedTopicTags.map((tag, idx) => (
             <span key={idx} className={[styles.Tag].join(' ')}>
               <FaHashtag />
-              {tag}
+              {tag.replace(/,/g, ' ').trim()}
             </span>
           ))}
         </span>
